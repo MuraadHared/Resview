8f7ee944e9f375156dd45d1cf9dc94aa44833002
Muraad <mhare095@uottawa.ca>
2015-03-12 16:38:45 -0400

    added Rater table to Tables.sql

--- sql/Tables.sql
+++ sql/Tables.sql
@@ -31,49 +31,12 @@ CREATE TABLE MenuItem
 	RestaurantID int REFERENCES Restaurant(RestaurantID) ON DELETE CASCADE
 );
 
-INSERT INTO Restaurant
-VALUES
-(DEFAULT, 'The Whalesbone Oyster House', 'Seafood', 'http://www.thewhalesbone.com/');
-INSERT INTO Restaurant
-VALUES
-(DEFAULT, 'Hokkaido Sushi', 'Japanese', 'www.hokkaidosushiottawa.ca');
-INSERT INTO Restaurant
-VALUES
-(DEFAULT, 'Host India', 'Indian', 'http://hostindia.ca');
-INSERT INTO Restaurant
-VALUES
-(DEFAULT, 'Baan Thai', 'Thai', 'http://www.baanthairestaurantottawa.ca/');
-INSERT INTO Restaurant
-VALUES
-(DEFAULT, 'Nando''s', 'Portugese', 'http://www.nandos.ca/');
-INSERT INTO Restaurant
-VALUES
-(DEFAULT, 'Soca Kitchen & Pub', 'Spanish', 'http://www.socapub.com/');
-INSERT INTO Restaurant
-VALUES
-(DEFAULT, 'Haveli', 'Indian', 'http://www.haveli.com/');
-INSERT INTO Restaurant
-VALUES
-(DEFAULT, 'Mucho Burrito', 'Mexican', 'http://muchoburrito.com/');
-INSERT INTO Restaurant
-VALUES
-(DEFAULT, 'Shawarma Prince', 'Middle Eastern', 'http://princegourmet.com/');
-INSERT INTO Restaurant
-VALUES
-(DEFAULT, 'East Side Mario''s', 'Italian', 'http://www.eastsidemarios.com/');
-INSERT INTO Restaurant
-VALUES
-(DEFAULT, 'Red Lobster', 'Seafood', 'http://www.redlobster.ca/');
-INSERT INTO Restaurant
-VALUES
-(DEFAULT, 'Pho Thu Do', 'Vietnamese', 'http://phothudo.com/');
-INSERT INTO Restaurant
-VALUES
-(DEFAULT, 'Vittoria Trattoria', 'Italian', 'https://www.vittoriatrattoria.com/');
-INSERT INTO Restaurant
-VALUES
-(DEFAULT, 'Lone Star Texas Grill', 'Mexican', 'http://www.lonestartexasgrill.com/');
-INSERT INTO Restaurant
-VALUES
-(DEFAULT, 'Silk Road Kabob House', 'Middle Eastern', 'http://silkroadkabobhouse.com/');
-SELECT * FROM Restaurant;
+CREATE TABLE Rater 
+(	
+	UserID serial PRIMARY KEY,
+	email CHAR (20),
+	name CHAR (15), 
+	join_date DATE,
+	type integer CHECK(integer >=1 AND integer <=5) DEFAULT 1,
+	reputation CHAR (15)
+); 
