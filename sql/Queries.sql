9605847cc0ad4afc2938b2165e995b884edee0e0
Muraad <mhare095@uottawa.ca>
2015-03-29 21:26:01 -0400

    Muraad's five beautiful queries. However, my last one might need some testing. Couldn't properly add data to test with

---
+++ sql/Queries.sql
@@ -0,0 +1,34 @@
+--Queries--
+
+--a--
+SELECT *
+FROM RESTAURANT R
+WHERE R.name = "";
+
+--b--
+SELECT M.name, M.price
+FROM MenuItem M
+WHERE M.RestaurantID = "";
+ORDER BY M.category;
+
+--c--
+
+SELECT L.manager_name, L.open_date
+FROM LOCATION L, RESTAURANT R
+WHERE L.RestaurantID = R.RestaurantID AND R.type = "PARAMETER";
+
+--D--
+SELECT M.name, L.manager_name, L.hour_open, l.hour_close, R.url
+FROM LOCATION L , RESTAURANT R, MenuItem M
+WHERE R.name="PARAMETER" AND R.RestaurantID=M.RestaurantID AND  L.RestaurantID=R.RestaurantID AND
+M.PRICE >= ALL 
+(
+SELECT M.PRICE
+FROM MenuItem M; );
+
+
+--E ***NEEDS TESTING*--
+SELECT R.type, M.category, AVG(M.price) AS Average_Price
+FROM RESTAURANT R, MenuItem M
+WHERE M.RestaurantID = R.RestaurantID
+GROUP BY M.category, R.type;
