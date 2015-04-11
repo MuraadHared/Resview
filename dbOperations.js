b8ab6379268ee6380c98b41abed5fd4963420d49
Sarmad Hashmi <shash034@uottawa.ca>
2015-04-11 04:19:13 -0400

    sarmad's huge commit

--- dbOperations.js
+++ dbOperations.js
@@ -1,10 +1,29 @@
 // DB access operations
 
 var pg = require('pg');
-var connection = "postgres://postgres:admin@localhost:5432/postgres";
+var connection = "postgres://postgres:2132@localhost:5432/RestaurantProject";
 // req holds all the data that was entered from the view (ex: req.query.firstName)
 // 
 module.exports = {
+    getRaters: function(req, res) {            
+        var client = new pg.Client(connection);
+        client.connect();
+
+        // build query
+        var query = client.query("select * from rater");
+
+        // add rows obtained from query to result
+        query.on("row", function (row, result) { 
+            result.addRow(row);  //adds row to the result object (result object will have all the rows obtained from query after this)
+        });        
+        // once query has obtained all rows, decide what to do with result variable
+        query.on("end", function (result) {                      
+            client.end(); // disconnect client
+            res.writeHead(200, {'Content-Type': 'application/json'}); // prepare result for view (response header)
+            res.write(JSON.stringify(result.rows, null, "    ") + "\n"); // write the response
+            res.end();  // send content of response to the client and signal the server that the response has been sent
+        });
+  },
   getRestaurants: function(req, res) {            
         var client = new pg.Client(connection);
         client.connect();
@@ -21,6 +40,25 @@ module.exports = {
             client.end(); // disconnect client
             res.writeHead(200, {'Content-Type': 'application/json'}); // prepare result for view (response header)
             res.write(JSON.stringify(result.rows, null, "    ") + "\n"); // write the response
+            res.end();  // send content of response to the client and signal the server that the response has been sent
+        });
+  },
+  login: function(req, res) {
+        var client = new pg.Client(connection);
+        client.connect();
+        console.log(req.params);
+        // build query
+        var query = client.query("select * from rater WHERE name = '" + req.params.user + "'"); // modify with password
+
+        // add rows obtained from query to result
+        query.on("row", function (row, result) { 
+            result.addRow(row);  //adds row to the result object (result object will have all the rows obtained from query after this)
+        });        
+        // once query has obtained all rows, decide what to do with result variable
+        query.on("end", function (result) {                      
+            client.end(); // disconnect client            
+            res.writeHead(200, {'Content-Type': 'application/json'}); // prepare result for view (response header)
+            res.write(JSON.stringify(result.rows.length, null, "    ") + "\n"); // write the response
             res.end();  // send content of response to the client and signal the server that the response has been sent
         });
   },
@@ -67,7 +105,7 @@ module.exports = {
         client.connect();
 
         // build query
-        var query = client.query("SELECT L.open_date , R.name FROM Restaurant R, Location L, Rating RT WHERE L.RestaurantID = R.RestaurantID AND R.RestaurantID = RT.RestaurantID AND RT.staff < ( SELECT GREATEST (MAX(RT1.staff), MAX(RT1.food), MAX(RT1.price), MAX(RT1.mood)) FROM Rating RT1, Rater RA WHERE RT1.UserID = RA.UserID AND RA.name = 'User A')");
+        var query = client.query("SELECT L.open_date , R.name FROM Restaurant R, Location L, Rating RT WHERE L.RestaurantID = R.RestaurantID AND R.RestaurantID = RT.RestaurantID AND RT.staff < ( SELECT GREATEST (MAX(RT1.staff), MAX(RT1.food), MAX(RT1.price), MAX(RT1.mood)) FROM Rating RT1, Rater RA WHERE RT1.UserID = RA.UserID AND RA.name = '"+ req.params.raterName+"')");
 
         // add rows obtained from query to result
         query.on("row", function (row, result) { 
@@ -105,7 +143,7 @@ module.exports = {
         client.connect();
 
         // build query
-        var query = client.query("SELECT CASE WHEN ( SELECT COUNT(*) FROM Restaurant R, Rating RA WHERE R.type = 'Indian' AND RA.RestaurantID = R.RestaurantID) >  (SELECT COUNT(*) FROM Restaurant R, Rating RA WHERE R.type = 'Indian' AND RA.RestaurantID = R.RestaurantID)  THEN 'Type X restaurants are more popular than Type Y restaurants' ELSE 'Type X restaurants are NOT AS popular as Type Y restaurants' END AS popular");
+        var query = client.query("SELECT CASE WHEN ( SELECT COUNT(*) FROM Restaurant R, Rating RA WHERE R.type = '"+ req.params.morePopularX + "' AND RA.RestaurantID = R.RestaurantID) >  (SELECT COUNT(*) FROM Restaurant R, Rating RA WHERE R.type = '"+ req.params.morePopularY + "' AND RA.RestaurantID = R.RestaurantID)  THEN 'Type X restaurants are more popular than Type Y restaurants' ELSE 'Type X restaurants are NOT AS popular as Type Y restaurants' END AS popular");
 
         // add rows obtained from query to result
         query.on("row", function (row, result) { 
@@ -119,6 +157,7 @@ module.exports = {
             res.end();  // send content of response to the client and signal the server that the response has been sent
         });
   },
+
     getRatings : function(req, res){        
         var client = new pg.Client(connection);
         client.connect();        
@@ -127,6 +166,98 @@ module.exports = {
         query.on("row", function (row, result) { 
             result.addRow(row);
         });                
+
+        query.on("end", function (result) {            
+            client.end();
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.write(JSON.stringify(result.rows, null, "    ") + "\n");            
+            res.end();
+        });
+    },
+
+    //Query A
+    //incorrect query, need to add location data
+    selectRestaurant : function(req, res){        
+        var client = new pg.Client(connection);
+        client.connect();                
+        var query = client.query("SELECT * FROM RESTAURANT R where R.RestaurantID = " + req.params.id);    
+
+        query.on("row", function (row, result) { 
+            result.addRow(row);
+        });              
+
+        query.on("end", function (result) {                        
+            client.end();
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.write(JSON.stringify(result.rows, null, "    ") + "\n");            
+            res.end();
+        });
+    },
+
+    //Query B
+    selectMenuItem : function(req, res){        
+        var client = new pg.Client(connection);
+        client.connect();        
+        var query = client.query("SELECT M.name, M.price FROM MenuItem M , RESTAURANT R WHERE R.RestaurantID = "+ req.params.id +" AND M.RestaurantID = R.RestaurantID ORDER BY M.category" );    
+
+        query.on("row", function (row, result) { 
+            result.addRow(row);
+        });      
+
+        query.on("end", function (result) {            
+            client.end();
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.write(JSON.stringify(result.rows, null, "    ") + "\n");            
+            res.end();
+        });
+    },
+
+    //Query C
+    categoryRestaurant : function(req, res){        
+        var client = new pg.Client(connection);
+        client.connect();        
+        var query = client.query("SELECT R.name, L.manager_name, L.street_address, L.open_date  FROM LOCATION L, RESTAURANT R WHERE L.RestaurantID = R.RestaurantID AND R.type = '"+ req.params.type +"'");    
+
+        query.on("row", function (row, result) { 
+            result.addRow(row);
+        });      
+
+        query.on("end", function (result) {            
+            client.end();
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.write(JSON.stringify(result.rows, null, "    ") + "\n");            
+            res.end();
+        });
+    },
+
+    //Query D
+    expensiveMenuItem : function(req, res){        
+        var client = new pg.Client(connection);
+        client.connect();                
+        var query = client.query("SELECT M.name, L.manager_name, L.hour_open, l.hour_close, R.url FROM LOCATION L , RESTAURANT R, MenuItem M WHERE R.RestaurantID = "+ req.params.id +" AND R.RestaurantID=M.RestaurantID AND  L.RestaurantID=R.RestaurantID AND M.price >= ALL ( SELECT M.price FROM MenuItem M )");    
+
+        query.on("row", function (row, result) { 
+            result.addRow(row);
+        });      
+
+        query.on("end", function (result) {
+            console.log(result);
+            client.end();
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.write(JSON.stringify(result.rows, null, "    ") + "\n");            
+            res.end();
+        });
+    },
+
+    //Query E
+    averagePrices : function(req, res){        
+        var client = new pg.Client(connection);
+        client.connect();        
+        var query = client.query("SELECT R.type, M.category, AVG(M.price) AS Average_Price FROM RESTAURANT R, MenuItem M WHERE M.RestaurantID = R.RestaurantID GROUP BY M.category, R.type ORDER BY R.type");    
+
+        query.on("row", function (row, result) { 
+            result.addRow(row);
+        });      
 
         query.on("end", function (result) {            
             client.end();
--- public/js/core.js
+++ public/js/core.js
@@ -1,21 +1,59 @@
-var restaurantProject = angular.module('restaurantProject', ['ngRoute']);
-
-
+var restaurantProject = angular.module('restaurantProject', ['ngRoute', 'Authentication', 'ngCookies']);
 restaurantProject.config(['$routeProvider', function($routeProvider) {
-    $routeProvider
+    $routeProvider  
+        .when('/login', 
+            {
+                controller: 'LoginController',
+                templateUrl: 'html/login.html'
+            })
+        .when('/home', 
+            {
+                controller: 'RestaurantController',
+                templateUrl: 'html/home.html'
+            })
+        .when('/menus', 
+            {
+                controller: 'RestaurantController',
+                templateUrl: 'html/RestaurantsAndMenus.html'
+            })
         .when('/restaurants', 
             {
                 controller: 'RestaurantController',
-                templateUrl: 'html/restaurants.html'
+                templateUrl: 'html/RatingsofRestaurants.html'
             })
-        .otherwise({redirectTo: '/restaurants'});
+        .when('/raters', 
+            {
+                controller: 'RestaurantController',
+                templateUrl: 'html/RatersAndTheirRatings.html'
+            })                   
+        .otherwise({redirectTo: '/login'});
 }]);
 
-
-restaurantProject.controller('RestaurantController', function($scope, $http) {
+restaurantProject.controller('RestaurantController', ['$scope', '$rootScope', '$http','$location', function($scope, $rootScope, $http, $location) {        
+        // get default data. Restaurants, Raters, RestaurantTypes
         $http.get('/restaurant').success(function(data) {            
-                $scope.restaurantList = data;
+                $scope.restaurantList = data;                
+                var alreadyAdded = {};
+                var types = [];
+                data.forEach(function(r) {                    
+                    if (!alreadyAdded[r.type]) {
+                        alreadyAdded[r.type] = true;
+                        types.push(r.type);
+                    }
+                })
+                types.sort();
+                $scope.types = types;   
+                $scope.restaurant = $scope.restaurantList[0];
+                $scope.restaurantType = $scope.types[0];
+                $scope.restaurantTypeX = $scope.types[0];
+                $scope.restaurantTypeY = $scope.types[0];
         });
+
+         $http.get('/raters').success(function(data) {            
+                $scope.raterList = data;                                
+                $scope.raterName = $scope.raterList[0];                
+        });
+
 
         $scope.getRatings = function(restaurant) {            
             $http.get('/rating/' + restaurant.restaurantid).success(function(data) {                
@@ -23,40 +61,85 @@ restaurantProject.controller('RestaurantController', function($scope, $http) {
             });
         }
 
+        $scope.redirectToQuery = function(q) {            
+            eval("$scope."+ q +"()");            
+        }
+                                
+        $scope.A = function() {                        
+           $http.get('/A/' + $scope.restaurant.restaurantid).success(function(data) {                
+                $scope.selectR = data;                
+            });
+        }
 
-        $scope.f = function() {            
-            $http.get('/f').success(function(data) {                
-                $scope.f = data;
+        $scope.B = function() {            
+           $http.get('/B/' + $scope.restaurant.restaurantid).success(function(data) {                
+                $scope.bData = data;
+            });
+        }
+
+        $scope.C = function() {            
+           $http.get('/C/' + $scope.restaurantType).success(function(data) {                
+                $scope.cData = data;
+            });
+        }
+
+        $scope.D = function() {            
+           $http.get('/D/' + $scope.restaurant.restaurantid).success(function(data) {                
+                $scope.dData = data;
+            });
+        }
+
+        $scope.E = function() {            
+           $http.get('/E').success(function(data) {                
+                $scope.eData = data;
             });
         }
 
 
-        $scope.g = function() {            
-            $http.get('/g').success(function(data) {                
-                $scope.g = data;
-            });
-        }
-
-        $scope.h = function(restaurant) {            
-            $http.get('/h').success(function(data) {                
-                $scope.h = data;
-            });
-        }
-
-        $scope.i = function(restaurant) {            
-            $http.get('/i/'+ $scope.type).success(function(data) {                
-                $scope.i = data;
-            });
-        }
-
-        $scope.j = function(restaurant) {            
-           $http.get('/j').success(function(data) {                
-                $scope.j = data;
+       $scope.F = function() {            
+            $http.get('/F').success(function(data) {                
+                $scope.frestaurantQuery = data;
             });
         }
 
 
+        $scope.G = function() {            
+            $http.get('/G').success(function(data) {                
+                $scope.grestaurantQuery = data;
+            });
+        }
 
-}
+        $scope.H = function() {            
+            $http.get('/H/' + $scope.raterName).success(function(data) {                                
+                $scope.hrestaurantQuery = data;
+            });
+        }
 
-);
+        $scope.I = function() {            
+            $http.get('/I/'+ $scope.restaurantType).success(function(data) {                
+                $scope.irestaurantQuery = data;
+            });
+        }
+
+        $scope.J = function() {            
+           $http.get('/J/' + $scope.restaurantTypeX + '/' + $scope.restaurantTypeY).success(function(data) {                
+                $scope.jrestaurantQuery = data;
+            });
+        }
+}]);
+
+restaurantProject.run(['$rootScope', '$location', '$cookieStore', '$http',
+    function ($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+        }
+  
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in
+            if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+                $location.path('/login');
+        }
+    });
+}]);
